4402adb23a1a80325fe847db6782eb446bad6c7a
j <vanakozubenko49@outlook.com>
2023-03-02 04:26:25 -0500

    read from socket

--- http_server/main.cpp
+++ http_server/main.cpp
@@ -1,6 +1,32 @@
-#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-int main() {
-    printf("hello world");
+#define PORT 8080
+#define BUFFER_SIZE 50000
+
+int main(int argc, char *argv[]) {
+    const int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    const struct sockaddr_in server_info = {
+        .sin_family = AF_INET,
+        .sin_port = htons(PORT),
+        .sin_addr = htonl(INADDR_ANY),
+    };
+
+    bind(listenfd, (sockaddr *)&server_info, sizeof(server_info));
+
+    listen(listenfd, 32);
+
+    const int connectfd = accept(listenfd, nullptr, nullptr);
+
+    char read_buffer[BUFFER_SIZE] = {0};
+
+    read(connectfd, read_buffer, BUFFER_SIZE);
+
+    std::cout << read_buffer;
+
     return 0;
 }
